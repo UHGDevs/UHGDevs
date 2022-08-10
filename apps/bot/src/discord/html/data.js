9dbdcbba8946c76157bf60264991e7a6a540eb5e
Farmans <Farman52CZ@seznam.cz>
2022-08-10 21:40:56 +0200

    removed the html folder

--- apps/bot/src/discord/html/data.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-module.exports = {
-  test_index: fs.readFileSync(`src/discord/html/test/index.html`, 'utf8'),
-  test_css: fs.readFileSync(`src/discord/html/test/style.css`, 'utf8'),
-  test_backround: 'data:image/jpeg;base64,' + new Buffer.from(fs.readFileSync('src/discord/html/test/backround.png')).toString('base64')
-
-}
