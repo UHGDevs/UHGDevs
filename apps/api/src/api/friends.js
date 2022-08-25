d87b9cb0ae2e40f8cbc1e684b283bb7c553af0c2
Farmans <Farman52CZ@seznam.cz>
2022-08-25 18:35:07 +0200

    Friends API

---
+++ apps/api/src/api/friends.js
@@ -0,0 +1,29 @@
+
+const func = require('../util/ApiFunctions');
+
+class Friends {
+
+  static async call(options) {
+    const client = options.client;
+
+    if (!options.uuid) return {success: false, type: "friends", reason: 'Friends API needs player UUID or friends NAME to be called!'};
+    const uuid = options.uuid;
+
+    const apikey = client.getKey()
+    if (!apikey) return  {success: false, type: "friends", reason: `Hypixel API key not found`};
+
+    const limit = client.ratelimit();
+    if (limit <= 0) return {success: false, type: "friends", reason: 'Hypixel API key limit reached!'};
+
+    let friends;
+    try { friends = await client.callHypixel.get('friends', {params: { key: apikey, player: uuid }}).then( n => n.data ) } catch (e) {return {success: false, type: "friends", reason: e.response ? e.response.data.cause : 'Friends API error'}};
+    if (!friends.success) return  {success: false, type: "friends", reason: friends.cause};
+
+    const api = { success: true, type: 'friends', friends: friends.records }
+
+
+    return api
+  }
+}
+
+module.exports = Friends;
