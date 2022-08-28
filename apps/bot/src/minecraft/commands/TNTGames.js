861a6fd04d785974ced6379367bc280cb510b9e1
Farmans <Farman52CZ@seznam.cz>
2022-08-28 14:05:34 +0200

    tntgames api & cmd

---
+++ apps/bot/src/minecraft/commands/TNTGames.js
@@ -0,0 +1,18 @@
+module.exports = {
+    name: "TNTGames",
+    aliases: ["tntgames", "tnt", "tnts", "tntgame", "wizard", "wizards", "tntwizard", "tntwizards", "tntrun", "pvprun", "tnttag", "bowspleef", "tntbowspleef", "tntspleef"],
+    run: async (uhg, pmsg) => {
+      try{
+        let api = await uhg.api.call(pmsg.nickname)
+        if (!api.success) return api.reason
+        let tnt = api.hypixel.stats.tntgames
+        let overall = tnt.overall
+        let message = `**TNTGames**: **${api.username}** - ${uhg.f(overall.wins)}Wins ${uhg.f(overall.kills)}Kills ${uhg.f(overall.kdr)}KDR (Playtime: ${Math.floor(tnt.playtime/60)}h)`
+        return message
+      } catch (e) {
+          console.log(String(e.stack).bgRed)
+          return "Chyba v TNTGames příkazu!"
+      }
+    }
+  }
+  
