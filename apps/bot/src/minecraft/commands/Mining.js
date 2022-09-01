af8f88dba92aa91c1d224c78db0778233c97bb5f
DavidCzPdy <dakolencik@gmail.com>
2022-09-01 20:56:22 +0200

    Mining command adjustment

--- apps/bot/src/minecraft/commands/Mining.js
+++ apps/bot/src/minecraft/commands/Mining.js
@@ -12,7 +12,23 @@ module.exports = {
       let profil = cute_name ? api.skyblock.profiles.find(n => n.name.toLowerCase() == cute_name) : api.skyblock.profiles[0]
       if (!profil) return "Neplatný profil"
       //let message = `Mining: **${api.username}** - HOTM Tier ${profil.member.mining.hotm_tier} | ${f(profil.member.mining.nucleus)} Nucleus Run${profil.member.mining.nucleus == 1 ? '':'s' } | ${f(profil.member.mining.scatha)} Scatha Kill${profil.member.mining.scatha == 1 ? '':'s'} | ${f(profil.member.mining.commissions)} Comission${profil.member.mining.commissions == 1 ? '':'s'} | ${f(profil.member.mining.powder_mithril)} Mithril Powder | ${f(profil.member.mining.powder_gemstone)} Gemstone Powder`
-      let message = `Mining:**${profil.member.skills_api ? ` [${uhg.f(profil.member.skills.mining.level)}]`:''} ${api.username}** - HOTM ${profil.member.mining.hotm_tier} | ${f(profil.member.mining.nucleus)} Nucleus | ${f(profil.member.mining.scatha)} Scatha | ${f(profil.member.mining.commissions)} Comission${profil.member.mining.commissions == 1 ? '':'s'} | Powder: ${uhg.money(profil.member.mining.powder_mithril)} Mithril, ${uhg.fPowder(profil.member.mining.powder_gemstone)} Gemstone | Collection: ${uhg.fPowder(profil.member.collection.gemstone_collection)} Gemstone, ${uhg.money(profil.member.collection.hard_stone)} HardStone`
+      let cmd = [
+        'Mining:**',
+        profil.member.skills_api ? ` [${uhg.f(profil.member.skills.mining.level)}]` : null,
+        ' ' + api.username + '**',
+        ` - HOTM ${profil.member.mining.hotm_tier}`,
+        profil.member.mining.nucleus ? ` | ${f(profil.member.mining.nucleus)} Nucleus`: null,
+        profil.member.mining.scatha ? ` | ${f(profil.member.mining.scatha)} Scatha`: null,
+        profil.member.mining.commissions ? ` | ${f(profil.member.mining.commissions)} Comission${profil.member.mining.commissions == 1 ? '':'s'}`: null,
+        profil.member.mining.powder_mithril ? ` | Powder: ${uhg.fPowder(profil.member.mining.powder_mithril)} Mithril`: null,
+        profil.member.mining.powder_gemstone ? `, ${uhg.fPowder(profil.member.mining.powder_gemstone)} Gemstone`: null,
+        profil.member.collection.gemstone_collection || profil.member.collection.hard_stone ? ' | Collection:' : null,
+        profil.member.collection.gemstone_collection ? ` ${uhg.money(profil.member.collection.gemstone_collection)} Gemstone` : null,
+        profil.member.collection.gemstone_collection && profil.member.collection.hard_stone ? ',': null,
+        profil.member.collection.hard_stone ? ` ${uhg.money(profil.member.collection.hard_stone)} HardStone` : null,
+      ].filter(n => n)
+      let message = cmd.join('')
+      //let message = `Mining:**${profil.member.skills_api ? ` [${uhg.f(profil.member.skills.mining.level)}]`:''} ${api.username}** - HOTM ${profil.member.mining.hotm_tier} | ${f(profil.member.mining.nucleus)} Nucleus | ${f(profil.member.mining.scatha)} Scatha | ${f(profil.member.mining.commissions)} Comission${profil.member.mining.commissions == 1 ? '':'s'} | Powder: ${uhg.money(profil.member.mining.powder_mithril )} Mithril${profil.member.mining.powder_gemstone ? ', ' + uhg.fPowder(profil.member.mining.powder_gemstone) + ' Gemstone': ''} | Collection: ${uhg.fPowder(profil.member.collection.gemstone_collection || 0)} Gemstone${profil.member.collection.hard_stone ? ', ' + uhg.money(profil.member.collection.hard_stone) + ' HardStone' : ''}`
       return message
     } catch (e) {
         console.log(String(e.stack).bgRed)
