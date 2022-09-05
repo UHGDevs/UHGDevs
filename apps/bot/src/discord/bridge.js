85f027eee9e5b75c05bc6103cfa40b6088aeb8d0
DavidCzPdy <dakolencik@gmail.com>
2022-09-05 18:12:23 +0200

    new config "offline"
    Open visual studio - config.json, fetch, in visual studio in config.json press ctrl + z, add offline: true and save

--- apps/bot/src/discord/bridge.js
+++ apps/bot/src/discord/bridge.js
@@ -6,6 +6,9 @@ module.exports = async (uhg, message) => {
   if (message.channel.id==uhg.getDiscordIds().channels.guild) chat = "/gc"
   if (!chat) return;
 
+  if (socket._host !== 'mc.hypixel.net') return uhg.settings.offline ? message.reply({ content: "Bot není na hypixelu.", failIfNotExists: false }) : false
+  console.log(uhg.mc.client)
+
   let user = uhg.data.verify?.length ? uhg.data.verify.find(n=>n._id==message.author.id) : {nickname: message.guild.members.cache.get(message.author.id).nickname || message.author.username}
   if (!user) return message.reply({ content: `Nejsi verifikovaný, zpráva nebyla odeslána.\nVerifikuj se pomocí ${uhg.settings.prefix}verify *\`nick\`*`, failIfNotExists: false })
   if (message.content == "") return message.reply({ content: "Zpráva nelze odeslat", failIfNotExists: false})
