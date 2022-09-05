8dd41f9129bd994a21be654c26eff4533d555097
DavidCzPdy <dakolencik@gmail.com>
2022-09-05 22:09:29 +0200

    bridge feature

--- apps/bot/src/discord/bridge.js
+++ apps/bot/src/discord/bridge.js
@@ -18,8 +18,14 @@ module.exports = async (uhg, message) => {
   let username = user.nickname
   if (message.reference) {
     let msg = await uhg.dc.client.channels.cache.get(message.reference.channelId).messages.fetch(message.reference.messageId)
-    let replied = (uhg.data.verify?.length ? uhg.data.verify.find(n=>n._id==msg.author.id) : null) ||  {nickname: msg.guild.members.cache.get(msg.author.id).nickname || msg.author.username}
-    
+    let replied = {};
+    if (msg.author.id == '950076450478899270' || msg.author.id == '892275591682883604') {
+      if (msg.embeds?.length) replied.nickname = `${msg.embeds[0]?.title || ''} command`.trim()
+      else if (msg.content && msg.content.match(/.*(<:server1:914915236035825694><:server2:914915235956138014><:server3:914915235507339295><:server4:914916963304742982><:server5:914916963388649492> `).*(left.)/)) replied.nickname = 'left message'
+      else if (msg.content && msg.content.match(/.*(<:server1:914915236035825694><:server2:914915235956138014><:server3:914915235507339295><:server4:914916963304742982><:server5:914916963388649492> `).*(joined.)/)) replied.nickname = 'joined message'
+      else if (msg.content && msg.content.match(/\_.*\*\*(.*:)?\*\*/gi)) replied.nickname = msg.content.match(/\_.*\*\*(.*:)?\*\*/gi)[0].split('**').filter(n=>!n.includes(' '))[0]?.replace(':', '')
+      else replied.nickname = 'bot'
+    } else replied = (uhg.data.verify?.length ? uhg.data.verify.find(n=>n._id==msg.author.id) : null) ||  {nickname: msg.guild.members.cache.get(msg.author.id).nickname || msg.author.username}
     username = `${username} replied to ${replied.nickname}`
   }
 
