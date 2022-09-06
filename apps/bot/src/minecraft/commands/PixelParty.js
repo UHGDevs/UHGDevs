8505ce6e67d7d41d40958799016494ef762b6900
Farmans <Farman52CZ@seznam.cz>
2022-09-06 21:18:32 +0200

    Update PixelParty.js

--- apps/bot/src/minecraft/commands/PixelParty.js
+++ apps/bot/src/minecraft/commands/PixelParty.js
@@ -6,7 +6,7 @@ module.exports = {
         let api = await uhg.api.call(pmsg.nickname)
         if (!api.success) return api.reason
         let pp = api.hypixel.stats.arcade.pixelparty
-        let message = `**PixelParty**: **${api.username}** - ${uhg.f(pp.wins)}Wins ${uhg.f(pp.wlr)}WLR | ${uhg.f(pp.rounds)} Rounds (Highest ${pp.highestround}) | ${uhg.f(pp.powerups)} Powerups`
+        let message = `**PixelParty**: **${api.username}** - ${uhg.f(pp.wins || 0)}Wins ${uhg.f(pp.wlr || 0)}WLR | ${uhg.f(pp.rounds || 0)} Rounds (Highest ${pp.highestround || 0}) | ${uhg.f(pp.powerups || 0)} Powerups`
         return message
       } catch (e) {
           console.log(String(e.stack).bgRed)
