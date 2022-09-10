61b81184cf10fafbfab7081b0dc820f34afd397a
DavidCzPdy <dakolencik@gmail.com>
2022-09-10 18:27:44 +0200

    stop test

--- apps/bot/src/discord/commands/version.js
+++ apps/bot/src/discord/commands/version.js
@@ -8,17 +8,6 @@ module.exports = {
   platform: "dc",
   run: async (uhg, message, content) => {
     try {
-      
-
-      await uhg.delay(500)
-      console.time('API')
-      await hypixel.getKeyInformations(process.env.api_key);
-      console.timeEnd('API')
-
-      console.time('oldTIME')
-      await uhg.api.call('DavidCzPdy', ['hypixel'])
-      console.timeEnd('oldTIME')
-      
       let config = require('../../../package.json')
       return `Verze bota je ${config.version}`
     } catch (e) {
