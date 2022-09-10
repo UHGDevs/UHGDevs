642f701a52ffc66cb20d00eee124a78f2be37462
DavidCzPdy <dakolencik@gmail.com>
2022-09-10 18:40:27 +0200

    trying

--- apps/bot/src/discord/commands/version.js
+++ apps/bot/src/discord/commands/version.js
@@ -1,6 +1,12 @@
 const fs = require('fs');
 
-let hypixel = require('hypixel-api-nodejs')
+const axios = require('axios');
+const https = require('https')
+const httpsAgent = new https.Agent({ keepAlive: true });
+const instance = axios.create({
+  baseURL: 'https://api.hypixel.net/',
+  httpsAgent,
+})
 module.exports = {
   name: "version",
   aliases: ["verze"],
@@ -8,6 +14,13 @@ module.exports = {
   platform: "dc",
   run: async (uhg, message, content) => {
     try {
+      
+      console.time('API')
+      console.log(httpsAgent)
+      await instance.get('player', {params: { key: process.env.api_key, uuid: 'f50e5d5cca524c2ebc9d040acefa7c5a' }})
+     
+     console.timeEnd('API')
+
       let config = require('../../../package.json')
       return `Verze bota je ${config.version}`
     } catch (e) {
