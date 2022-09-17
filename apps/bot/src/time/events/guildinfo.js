b8e694c30699e4732803f3c04815b8edc551e804
DavidCzPdy <dakolencik@gmail.com>
2022-09-17 16:32:38 +0200

    new guild db refreshment

--- apps/bot/src/time/events/guildinfo.js
+++ apps/bot/src/time/events/guildinfo.js
@@ -140,6 +140,84 @@ module.exports = {
               )
         channel.send({ embeds: [emsend] })
       }
+
+if (date.getMinutes() !== 50) return
+      let nicknames = false
+
+      for (let db of ['uhg', 'tkjk']) {
+  
+          let guild = await uhg.api.call(db == 'uhg' ? '64680ee95aeb48ce80eb7aa8626016c7' : '574bfb977d4c475b8197b73b15194a2a', ['guild'])
+          if (!guild.success) continue;
+          guild = guild.guild?.guild
+          if (guild.name !== 'UltimateHypixelGuild' && guild.name !== 'TKJK') continue;
+  
+          let today = Object.keys(guild.members[0].expHistory)[0]
+          let yesterday = Object.keys(guild.members[0].expHistory)[1]
+  
+         // let temp = await uhg.get('stats', 'guild', {name: db == 'uhg' ? 'UltimateHypixelGuild' : 'TKJK'}).then(n => n[0] || {} )
+  
+          /* -- Guild GEXP -- */
+          let info = await uhg.mongo.run.get(db, 'info', {_id: 'xp'}).then(n => n[0] || {_id: 'xp', updated: null, total: 0, total_daily: {}, daily: {} })
+  
+          info.updated = new Date().getTime()
+          info.total = guild.exp
+          info.total_daily[today] = guild.exp
+          info.daily[today] = guild.exp - (info.total_daily[yesterday] || 0)
+  
+          await uhg.mongo.run.post(db, 'info', info)
+  
+          let members = await uhg.get(db, 'members', {})
+          let left = await uhg.get(db, 'left', {})
+  
+          let refreshed = false
+          /* -- Handle left MEMBERS --*/
+  
+          for (member of members?.filter(n => !guild.members.map(a => a.uuid).includes(n.uuid))) {
+              await uhg.mongo.run.post(db, 'left', member)
+              await uhg.mongo.run.delete(db, 'members', {_id: member._id})
+              refreshed = true
+          }
+  
+          /* -- Handle joined MEMBERS -- */
+          for (member of guild.members.filter(n => !members.map(a => a.uuid).includes(n.uuid))) {
+              let user = left.find(n => n.uuid == member.uuid) || { _id : member.uuid, uuid: member.uuid, firstJoined: member.joined, gexp: {} }
+              await uhg.mongo.run.post(db, 'members', user)
+              await uhg.mongo.run.delete(db, 'left', {_id: user._id})
+              refreshed = true
+          }
+  
+          if (refreshed) {
+              members = await uhg.mongo.run.get(db, 'members', {})
+              left = await uhg.mongo.run.get(db, 'left', {})
+          }
+  
+          /* -- Update MEMBERS --*/
+          for (let member of members) {
+              let api = guild.members.find(n => n.uuid = member.uuid)
+              member.joined = api.joined
+              member.quests = api.questParticipation
+  
+              if (!member.username || nicknames) {
+                  let user = await uhg.api.call(member.uuid, ['mojang'])
+                  let username = user.sucess ? user.username : member.username
+                  member.username = username
+              }
+  
+              member.gexp = Object.assign({}, member.expHistory, member.gexp, member.expHistory);
+  
+              await uhg.mongo.run.post(db, 'members', member)
+          }
+  
+          /* -- Update left MEMBERS --*/
+          for (let member of left) {
+              if (!member.username || nicknames) {
+                  let user = await uhg.api.call(member.uuid, ['mojang'])
+                  let username = user.sucess ? user.username : member.username
+                  member.username = username
+              }
+              await uhg.mongo.run.post(db, 'left', member)
+          }
+        }
       
     } catch(e) {
       if (uhg.dc.cache.embeds) uhg.dc.cache.embeds.timeError(e, eventName);
