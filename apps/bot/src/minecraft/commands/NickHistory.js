72d9d386b5c4f95a36f782b89656c195e8f52bd4
Farmans <Farman52CZ@seznam.cz>
2022-09-20 20:36:35 +0200

    Mojang removed nick history from api

--- apps/bot/src/minecraft/commands/NickHistory.js
+++ apps/bot/src/minecraft/commands/NickHistory.js
@@ -3,6 +3,7 @@ module.exports = {
   aliases: ["nickhistory", "namehistory", "history", "names", "nicks"],
   run: async (uhg, pmsg) => {
     try{
+      return "Mojang vymazal Nickname History z API, tudíž tato feature je mimo provoz"
       let api = await uhg.api.call(pmsg.nickname, ['mojang'], {verify: pmsg.verify_data})
       if (!api.success) return api.reason
       let message = `**${api.username}** - ${api.names.join(", ")}`
