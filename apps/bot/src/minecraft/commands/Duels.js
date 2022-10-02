77c0bff4e814484d08500054188180946275bacd
Farmans <Farman52CZ@seznam.cz>
2022-10-02 10:29:35 +0200

    duels cmd - winstreak api

--- apps/bot/src/minecraft/commands/Duels.js
+++ apps/bot/src/minecraft/commands/Duels.js
@@ -8,7 +8,10 @@ module.exports = {
         let api = await uhg.api.call(nickname)
         if (!api.success) return api.reason
         let duels = api.hypixel.stats.duels
-        let message = `**Duels**: **${api.username}** - ${f(duels.wins)}Wins ${f(duels.wlr)}WLR - ${f(duels.winstreak)} Winstreak (Best Winstreak: ${f(duels.bestwinstreak)})`
+
+        let winstreak = duels.wins > 0 && duels.bestwinstreak == 0 ? "(Winstreak API OFF)" : `- ${f(duels.winstreak)} Winstreak (Best Winstreak: ${f(duels.bestwinstreak)})`
+
+        let message = `**Duels**: **${api.username}** - ${f(duels.wins)}Wins ${f(duels.wlr)}WLR ${winstreak}`
         return message
       } catch (e) {
           console.log(String(e.stack).bgRed)
