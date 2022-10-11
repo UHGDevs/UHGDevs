4d589367624b9017df9f4f3e586c2682b488b14f
DavidCzPdy <dakolencik@gmail.com>
2022-10-11 18:30:45 +0200

    forgotten file

--- apps/guild/src/features/lootboxes/handler.js
+++ apps/guild/src/features/lootboxes/handler.js
@@ -1,11 +1,28 @@
 
 const path = require('node:path');
 const fs = require('fs');
+const { Collection } = require('discord.js');
 
-class Boxes {
+class LootBoxes {
     constructor() {
+        this.embeds = JSON.parse(fs.readFileSync(path.join(__dirname, './embeds.json'), 'utf8'));
+        //fs.watchFile(path.resolve(__dirname, './embeds.json'), () => this.refreshEmbeds());
 
+        this.loot_tables = JSON.parse(fs.readFileSync(path.join(__dirname, './data/loot_tables.json'), 'utf8'));
+        this.data = JSON.parse(fs.readFileSync(path.join(__dirname, './data/data.json'), 'utf8'));
+
+        this.emojis = new Collection()
+        for (let emoji in this.data.emoji) {
+            emoji = this.data.emoji[emoji]
+            this.emojis.set(emoji.name, emoji.raw)
+        }
+
+        this.commands = new Collection()
+        fs.readdirSync(path.join(__dirname, './commands')).filter((file) => file.endsWith(".js")).forEach(n => { let h = require(`./commands/${n}`); this.commands.set(h.name, new h.class(this))})
+
+        //let cmd = this.commands.get('boxes').get({ box: 'bronze' })
+        //console.log(cmd)
     }
 }
 
-module.exports = {class: new Boxes(), name: 'boxes'}
+module.exports = {class: new LootBoxes(), name: 'loot'}
