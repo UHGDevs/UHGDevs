c341e3fabb21631bb5681654cc78a93392478f81
Farman52 <Farman52CZ@seznam.cz>
2022-11-16 19:51:01 +0100

    denick feature for premium

--- apps/api/src/api/mojang.js
+++ apps/api/src/api/mojang.js
@@ -16,18 +16,26 @@ class Mojang {
     finally {
       if (!call) return {success: false, reason: 'Error ve volání MOJANGU', type: 'mojang', input: input} 
       if (!call.data || call.data.error) {
-        if (options.premium) {
-            let oldname = client.data_verify.find(n => { if (n.names && n.names.find(a => a.toLowerCase() == input.toLowerCase())) return true; return n.uuid.toLowerCase() == input.toLowerCase()})
-            if (oldname) return { success: true, username: oldname.nickname, uuid: oldname.uuid, names: oldname.names, type: 'mojang', textures: oldname.textures, created_at: oldname.date };
+        if (1==1) {
+            //let oldname = client.data_verify.find(n => { if (n.names && n.names.find(a => a.toLowerCase() == input.toLowerCase())) return true; return n.uuid.toLowerCase() == input.toLowerCase()})
+            //if (oldname) return { success: true, username: oldname.nickname, uuid: oldname.uuid, names: oldname.names, type: 'mojang', textures: oldname.textures, created_at: oldname.date };
+            if (client.options.antisniper) {
+              let antisniper = await axios.get(`https://api.antisniper.net/denick?key=${client.options.antisniper}&nick=${input}`)
+              if (antisniper.success) return callAgain(options, antisniper.player.ign, antisniper.queried_nick)
+            }
         }
         return {success: false, reason: call.data?.reason|| (input.length > 16 ? 'Neplatné UUID' : 'Neplatné jméno'), type: 'mojang', input: input} 
       }
       let mojang = call.data
       if (mojang.username == 'Smolda') mojang.date = "2016-12-29"
-      return { success: true, username: mojang.username, uuid: mojang.uuid.replace(/-/g, ''), names: mojang.username_history.map(n => n.username), textures: mojang.textures, created_at: mojang.created_at, type: 'mojang' };
-      
+      return { success: true, username: mojang.username, uuid: mojang.uuid.replace(/-/g, ''), names: mojang.username_history.map(n => n.username), textures: mojang.textures, created_at: mojang.created_at, nicked: options.oldnick || undefined, type: 'mojang' };      
     }
   }     
+  static async callAgain(options, username, oldnick) {
+    options.username = username
+    options.oldnick = oldnick
+    return this.call(options)
+  }
 }
 
 module.exports = Mojang;
