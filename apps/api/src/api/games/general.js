897d078ead2437c3f38838695e948e487e28f91a
Farman52 <Farman52CZ@seznam.cz>
2023-03-25 19:25:51 +0100

    username api quick hotfix

--- apps/api/src/api/games/general.js
+++ apps/api/src/api/games/general.js
@@ -11,7 +11,7 @@ module.exports = async (hypixel = {} , uuid, client) => {
     type: 'hypixel',
     _id: uuid,
     uuid: uuid,
-    username: await uhg.api.call(uuid || hypixel.displayname, ["mojang"]) || hypixel.displayname,
+    username: hypixel.displayname,
     rank: func.getRank(hypixel).rank,
     prefix: func.getRank(hypixel).prefix,
     color: func.getPlusColor(func.getRank(hypixel).rank, hypixel.rankPlusColor).hex,
