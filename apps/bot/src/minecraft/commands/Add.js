dac6bc0125d4242207467aa3739907a83bbdef21
Farman52 <Farman52CZ@seznam.cz>
2023-06-03 18:21:19 +0200

    add & remove mc staff cmds

---
+++ apps/bot/src/minecraft/commands/Add.js
@@ -0,0 +1,24 @@
+const { MessageEmbed, MessageActionRow, MessageSelectMenu } = require("discord.js");
+module.exports = {
+  name: "Add",
+  aliases: ["add"],
+  run: async (uhg, pmsg) => {
+    try {
+        const PERMS = ["Farmans", "Honzu", "unisdynasty", "DavidCzPdy", "0hBlood", "SolidL1m3", "Smolda", "Corruptedoslav", "macek2005"]
+        if (!PERMS.includes(pmsg.username)) return "Nemáš permise"
+        let args = pmsg.args
+        if (!args) return "Nezadal jsi jméno"
+        args = args.split(" ").filter(n => n)
+        if (!args.length) return "Nezadal jsi jméno"
+
+        let api = await uhg.getApi(args[0], ["key", "hypixel", "mojang"])
+        if (api instanceof Object == false) return api
+        uhg.mongo.run.post("stats", "stats", api.hypixel)
+        let message = `**${api.username}** byl přidán do databáze`
+        return message
+    } catch (e) {
+        console.log(String(e.stack).bgRed)
+        return "Chyba v Add příkazu!"
+    }
+  }
+}
---
+++ apps/bot/src/minecraft/commands/Remove.js
@@ -0,0 +1,24 @@
+const { MessageEmbed, MessageActionRow, MessageSelectMenu } = require("discord.js");
+module.exports = {
+  name: "Remove",
+  aliases: ["remove"],
+  run: async (uhg, pmsg) => {
+    try {
+        const PERMS = ["Farmans", "Honzu", "unisdynasty", "DavidCzPdy", "0hBlood", "SolidL1m3", "Smolda", "Corruptedoslav", "macek2005"]
+        if (!PERMS.includes(pmsg.username)) return "Nemáš permise"
+        let args = pmsg.args
+        if (!args) return "Nezadal jsi jméno"
+        args = args.split(" ").filter(n => n)
+        if (!args.length) return "Nezadal jsi jméno"
+
+        let api = await uhg.getApi(args[0], ["key", "hypixel", "mojang"])
+        if (api instanceof Object == false) return api
+        uhg.mongo.run.delete("stats", "stats", {_id: api.uuid})
+        let message = `**${api.username}** byl odebrán do databáze`
+        return message
+    } catch (e) {
+        console.log(String(e.stack).bgRed)
+        return "Chyba v Remove příkazu!"
+    }
+  }
+}
